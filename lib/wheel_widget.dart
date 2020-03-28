5c7cb513dd09905542653872342f5554d55fbbaa
Are Johan Tverrå <are@tverra.no>
2020-03-28 19:58:05 +0100

    Added launcher icon and support for landscape mode

--- lib/wheel_widget.dart
+++ lib/wheel_widget.dart
@@ -21,6 +21,14 @@ class _ChoreWheelWidgetState extends State<ChoreWheelWidget> {
   Widget build(BuildContext context) {
     FocusScope.of(context).requestFocus(FocusNode());
 
+    if (MediaQuery.of(context).orientation == Orientation.portrait) {
+      return portrait();
+    } else {
+      return landscape();
+    }
+  }
+
+  Widget portrait() {
     return Container(
       color: Theme.of(context).primaryColor.withAlpha(180),
       child: Column(
@@ -97,6 +105,88 @@ class _ChoreWheelWidgetState extends State<ChoreWheelWidget> {
     );
   }
 
+  Widget landscape() {
+    return Container(
+      color: Theme.of(context).primaryColor.withAlpha(180),
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: 40,
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+                height: 24,
+                color: Theme.of(context).primaryColor,
+              ),
+              Container(
+                child: Center(
+                    child: Icon(
+                  Icons.arrow_downward,
+                  size: 40,
+                )),
+              ),
+              WheelWidget(
+                ctrl,
+                widget.segments,
+                callbackFinished: (int segment) {
+                  if (widget.segments.length == 0) return;
+
+                  setState(() {
+                    isPlaying = false;
+                  });
+
+                  _showResult(segment);
+                },
+                callBackBefore: () {
+                  if (widget.segments.length == 0) return;
+
+                  setState(() {
+                    isPlaying = true;
+                  });
+                },
+                setController: ((handler) {
+                  ctrl = handler;
+                  return null;
+                }),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Column(),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            child: IconButton(
+              iconSize: 100,
+              color: Theme.of(context).primaryColor,
+              icon: Icon(
+                isPlaying
+                    ? Icons.pause_circle_outline
+                    : Icons.play_circle_outline,
+              ),
+              onPressed: () {
+                if (isPlaying) {
+                  ctrl.pause();
+
+                  setState(() {
+                    isPlaying = false;
+                  });
+                } else {
+                  ctrl.play();
+
+                  setState(() {
+                    isPlaying = true;
+                  });
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   void _showResult(int segment) {
     final Segment currentSegment = widget.segments[segment - 1];
     final ListItem currentListItem = widget.listItems[segment - 1];
@@ -142,50 +232,58 @@ class WheelWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double radius =
+        MediaQuery.of(context).orientation == Orientation.portrait
+            ? MediaQuery.of(context).size.width / 2.1
+            : MediaQuery.of(context).size.height / 2.5;
+    final double height =
+        MediaQuery.of(context).orientation == Orientation.portrait
+            ? MediaQuery.of(context).size.width - 20
+            : MediaQuery.of(context).size.height - 70;
+
     return Container(
-      height: MediaQuery.of(context).size.width - 20,
-      child: Center(
-        child: Winwheel(
-          handleCallback: ((handler) {
-            setController(handler);
-            return null;
-          }),
-          controller: controller,
-          outerRadius: MediaQuery.of(context).size.width / 2.1,
-          innerRadius: 15,
-          strokeStyle: Colors.white,
-          textFontSize: 16.0,
-          textFillStyle: Colors.red,
-          textFontWeight: FontWeight.bold,
-          textAlignment: WinwheelTextAlignment.center,
-          textOrientation: WinwheelTextOrientation.horizontal,
-          wheelImage: 'assets/planes.png',
-          drawMode: WinwheelDrawMode.code,
-          drawText: true,
-          imageOverlay: false,
-          textMargin: 30,
-          pointerAngle: 0,
-          pointerGuide: PointerGuide(
-            display: true,
+      height: height,
+      width: height,
+      child: Winwheel(
+        handleCallback: ((handler) {
+          setController(handler);
+          return null;
+        }),
+        controller: controller,
+        outerRadius: radius,
+        innerRadius: 15,
+        strokeStyle: Colors.white,
+        textFontSize: 16.0,
+        textFillStyle: Colors.red,
+        textFontWeight: FontWeight.bold,
+        textAlignment: WinwheelTextAlignment.center,
+        textOrientation: WinwheelTextOrientation.horizontal,
+        wheelImage: 'assets/planes.png',
+        drawMode: WinwheelDrawMode.code,
+        drawText: true,
+        imageOverlay: false,
+        textMargin: 30,
+        pointerAngle: 0,
+        pointerGuide: PointerGuide(
+          display: true,
+        ),
+        segments: segments,
+        pins: Pin(
+          // visible: true,
+          number: 16,
+          margin: 6,
+          // outerRadius: 5,
+          fillStyle: Colors.yellow,
+        ),
+        animation: WinwheelAnimation(
+          type: WinwheelAnimationType.spinToStop,
+          curve: Curves.easeOutExpo,
+          spins: 8,
+          duration: const Duration(
+            seconds: 10,
           ),
-          segments: segments,
-          pins: Pin(
-            // visible: true,
-            number: 16,
-            margin: 6,
-            // outerRadius: 5,
-            fillStyle: Colors.yellow,
-          ),
-          animation: WinwheelAnimation(
-            type: WinwheelAnimationType.spinToStop,
-            curve: Curves.easeOutExpo,
-            spins: 8,
-            duration: const Duration(
-              seconds: 10,
-            ),
-            callbackFinished: callbackFinished,
-            callbackBefore: callBackBefore,
-          ),
+          callbackFinished: callbackFinished,
+          callbackBefore: callBackBefore,
         ),
       ),
     );
